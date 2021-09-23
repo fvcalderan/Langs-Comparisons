b06f5a354a682ae0adeda443c5d08b2f593e8252
Felipe <fvcalderan@gmail.com>
2021-09-23 17:00:24 -0300

    feat: f90 solutions

---
+++ biggest_len/biggest_len.f90
@@ -0,0 +1,11 @@
+! fpelogia
+program biggest_len
+implicit none
+character(len=20) :: s1 = "Here" ,s2 = "are", s3 = "some", &
+    s4 = "extraordinary", s5 = "words"
+character(len=20), allocatable :: v(:)
+allocate(v(5))
+v = [s1, s2, s3, s4, s5]
+write(*,*) maxval(len_trim(v))
+deallocate(v)
+end program
