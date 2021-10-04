f74bc2dacdab091f1f2928cdcccd21ee2569b0dd
fvcalderan <fvcalderan@gmail.com>
2021-10-04 09:22:06 -0300

    refactor: remove tabs from Fortran code

--- nearest_neighbor/nn.f95
+++ nearest_neighbor/nn.f95
@@ -1,5 +1,5 @@
 Program NearestNeighbor
-    real, dimension(3) :: qpoint = (/3,6,5/)
-    real, dimension(3,3) :: points = transpose(reshape((/1,2,3,3,4,5,5,6,7/),(/3,3/)))
-    write(*,*) minloc(norm2(points-spread(qpoint, 1, size(points, 1)), 2))
+real, dimension(3) :: qpoint = (/3,6,5/)
+real, dimension(3,3) :: points = transpose(reshape((/1,2,3,3,4,5,5,6,7/),(/3,3/)))
+write(*,*) minloc(norm2(points-spread(qpoint, 1, size(points, 1)), 2))
 End Program NearestNeighbor
