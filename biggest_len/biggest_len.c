36d81dfd3576bc543981a3bafd406e7ed1ea5f11
fvcalderan <fvcalderan@gmail.com>
2021-10-05 19:18:04 -0300

    refactor: improvement in the C code

--- biggest_len/biggest_len.c
+++ biggest_len/biggest_len.c
@@ -5,7 +5,7 @@ int main() {
     int big = 0, act = 0;
     char *words[5] = {"Here", "are", "some", "extraordinary", "words"};
 
-    for (int i = 0; i < sizeof(words)/sizeof(char *); ++i)
+    for (int i = 0; i < sizeof(words)/sizeof(*words); ++i)
         big = (act = strlen(words[i])) > big ? act : big;
 
     printf("%d\n", big);
