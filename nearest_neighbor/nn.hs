6571b68f1ca047246a2c5b7299bb87da2ea7a01f
fvcalderan <fvcalderan@gmail.com>
2021-10-11 15:52:56 -0300

    refactor: Haskell solution

--- nearest_neighbor/nn.hs
+++ nearest_neighbor/nn.hs
@@ -1,3 +1,3 @@
 points = [[1, 2, 3], [3, 4, 5], [5, 6, 7]]
 qpoint = [3, 6, 5]
-snd $ minimum $ zip [sum [i**2 | i <- zipWith (-) p qpoint] | p <- points] [0.. length points]
+snd $ minimum $ zip [sum [i**2 | i <- zipWith (-) p qpoint] | p <- points] [0..]
